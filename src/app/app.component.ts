0b69f885d5649b33d4c1db40bf14a765a9a18dca
Shuai <szheng3@uci.edu>
2019-08-29 21:42:47 -0500

    loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Carol';
+  @ViewChild('loading', {static: true}) loading: ElementRef;
+
+  ngAfterViewInit(): void {
+    // this.loading.nativeElement.remove();
+
+    const elementById = document.getElementById('loading');
+    if (elementById != null) {
+      elementById.remove();
+    }
+
+  }
 }
